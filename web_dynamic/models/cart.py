c431a37a1d1608d0a0b930d411e118766266f523
Abel Tibeso <138770113+Abel-alx-github@users.noreply.github.com>
2024-05-11 20:45:29 +0300

    Update cart.py

--- web_dynamic/models/cart.py
+++ web_dynamic/models/cart.py
@@ -11,12 +11,6 @@ from sqlalchemy.orm import relationship
 class Cart(BaseModel, Base):
     """ define Cart classes"""
     __tablename__ = 'carts'
-    #user_id = Column(String(50), ForeignKey('users.id'), nullable=False)
-    # product_id = Column(String(50), ForeignKey('products.id'), nullable=False)
-    #quantity = Column(Integer, nullable=False)
-    #price = Column(Float, nullable=False)
-    #user = relationship('User', backref='carts')
-    #product = relationship('Product', backref='carts')
     items = relationship('CartItem', backref='cart')
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
