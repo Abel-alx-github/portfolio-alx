bfb3f8298f88a58c01185680d6e3367802e24756
Abel Tibeso <138770113+Abel-alx-github@users.noreply.github.com>
2024-05-11 20:47:11 +0300

    Update category.py

--- web_dynamic/models/category.py
+++ web_dynamic/models/category.py
@@ -4,14 +4,15 @@
 
 import models
 from models.base_model import BaseModel, Base
-# from models.product import Product
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 class Category(BaseModel, Base):
-    __tablename__ = "categories"  # Optional, but good practice for clarity
+   """ define class Category"""
+    __tablename__ = "categories" 
     name = Column(String(50), nullable=False, unique=True)
-    # products = relationship("Product")
+   
 
     def __init__(self, *args, **kwargs):
         """initializes product"""
